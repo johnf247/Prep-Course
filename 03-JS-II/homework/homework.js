237507a81371ba3f1a955a305a16d9af520c497a
johnf247 <johnfredy247@hotmail.com>
2022-01-31 21:26:17 -0500

    second

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -131,12 +131,12 @@ function operadoresLogicos(num1, num2, num3) {
   if(num1 < 0 || num2 < 0 || num3 < 0) 
        { return "Hay negativos"; }
   else if(num1 === 0 || num2 === 0 || num3 === 0)
-       { return "error"; }
+       { return "Error"; }
   else if(num1 > 0 && num1 > num2 && num1 > num3) 
        { return "Número 1 es mayor y positivo"; }
   else if(num3 > num1 && num3 > num2) 
        { return num3 + 1; }
-  else { return "false" }
+  else { return false }
 }
 
 function esPrimo(numero) {
