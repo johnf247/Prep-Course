cae58a94bd6346adf5160dcc6c828e664728f2d0
johnf247 <johnfredy247@hotmail.com>
2022-02-04 19:03:19 -0500

    second

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -80,6 +80,11 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var suma = 0;
+  for (var i = 0; i < numeros.length; i++) {
+  suma = suma + numeros[i];
+  }
+  return suma;
 }
 
 
@@ -87,6 +92,7 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  return agregarNumeros(resultadosTest) / resultadosTest.length;
 }
 
 
@@ -94,20 +100,37 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var maximo = numeros[0]
+  for(var i = 1; i < numeros.length; i++) {
+    if(numeros[i] > maximo) {
+      numeros = numeros[i];
+       }
+    }
+    return maximo;
 }
-
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if(arguments.length < 1) return 0;
+  var total = 1;
+  for(var i = 0; i < arguments.length; i++) {
+    total = total * arguments[i];
+     }
+  return total;
 }
-
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  let contador = 0;
+  for (let i = 1 ; i < arreglo.length; i++) {
+    if(arreglo[i] > 19) {
+      contador++
+    } 
+  }
+     return contador
 }
 
 
@@ -116,7 +139,10 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if (numeroDeDia === 1 || numeroDeDia === 7) { 
+      return "Es fin de semana";
+  }
+   return "Es dia Laboral";
 } 
 
 
@@ -124,6 +150,7 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  
   
 }
 
